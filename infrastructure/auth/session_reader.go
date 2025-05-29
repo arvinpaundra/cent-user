ecb3a1fb6f8d516f4f9e4283f3ed5c69c764afb2
arvinpaundra <arvinpaundra06@gmail.com>
2025-05-29 10:52:59 +0700

    force bind to model for every querying

--- infrastructure/auth/session_reader.go
+++ infrastructure/auth/session_reader.go
@@ -22,13 +22,13 @@ func NewSessionReaderRepository(db *gorm.DB) SessionReaderRepository {
 }
 
 func (r SessionReaderRepository) FindByRefreshToken(ctx context.Context, userId int64, refreshToken string) (entity.Session, error) {
-	var session entity.Session
+	var sessionModel model.Session
 
 	err := r.db.WithContext(ctx).
 		Model(&model.Session{}).
 		Select("id", "user_id", "refresh_token").
 		Where("user_id = ? AND refresh_token = ? AND deleted_at IS NULL", userId, refreshToken).
-		Take(&session).
+		Take(&sessionModel).
 		Error
 
 	if err != nil {
@@ -37,6 +37,13 @@ func (r SessionReaderRepository) FindByRefreshToken(ctx context.Context, userId 
 		}
 
 		return entity.Session{}, err
+	}
+
+	session := entity.Session{
+		ID:           sessionModel.ID,
+		UserId:       sessionModel.UserId,
+		AccessToken:  sessionModel.AccessToken,
+		RefreshToken: sessionModel.RefreshToken.Ptr(),
 	}
 
 	return session, nil
